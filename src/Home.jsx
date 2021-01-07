9718dccf11198787166aa7f03d69d84d43b8e077
mic7x4 <mic7x4@gmail.com>
2021-01-07 17:32:26 +0200

    features : usestates

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react';
 
 function Home() {
+    const [name,setName] = useState('Crookzart');
+    const [age,setAge] = useState(24);
+
     const handleClick = () => {
-        console.log('Hello, Netphantom');
+        setName('Netphantom');
+        setAge(40);
+
     }
     return (
         <div className="home"> 
             <h1>Home Page</h1>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     )
