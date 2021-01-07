35f2e3f5565c46caf69352e98d09f24f9c8d8428
mic7x4 <mic7x4@gmail.com>
2021-01-07 19:16:54 +0200

    features: adding post into usestate

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,20 @@
 import React,{useState} from 'react';
 
 function Home() {
-    const [name,setName] = useState('Crookzart');
-    const [age,setAge] = useState(24);
 
-    const handleClick = () => {
-        setName('Netphantom');
-        setAge(40);
-
-    }
+ const [blogs,setBlogs] = useState([
+     {title:'My new Website', body:"lorem ipsom.....",author:'crook',id:1},
+     {title:'Welcome to the party', body:"lorem ipsom.....",author:'pop Smoke',id:2},
+     {title:'Webdev Top Tips', body:"lorem ipsom.....",author:'Ninja',id:3},
+ ]);
     return (
         <div className="home"> 
-            <h1>Home Page</h1>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) =>(
+                <div className="blog__preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.author}</p>
+                    </div>
+            ) )}
         </div>
     )
 }
