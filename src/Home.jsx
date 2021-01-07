89077421146dd5f2d9ab2c328fa542f47337fbe0
mic7x4 <mic7x4@gmail.com>
2021-01-07 20:52:40 +0200

    [features] using useEffect hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import BlogList from './BlogList';
 
 function Home() {
@@ -13,6 +13,11 @@ const handleDelete = (id) => {
     const newBlog = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlog);
 }
+
+useEffect(() => {
+ console.log('Use Effect runs==>');
+ console.log(blogs)
+});
 
     return (
         <div className="home">
