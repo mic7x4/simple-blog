7a28373572c8ac9fa9222002d5c3e4f7bc4b7f3d
mic7x4 <mic7x4@gmail.com>
2021-01-12 18:16:06 +0200

    [chore] handling the errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,26 +5,34 @@ function Home() {
 
  const [blogs,setBlogs] = useState(null);
  const [isPending, setIsPending] = useState(true);
-
-// const handleDelete = (id) => {
-//     const newBlog = blogs.filter((blog) => blog.id !== id);
-//     setBlogs(newBlog);
-// }
+ const [error,setError] = useState(null);
 
 useEffect(() => {
+    setTimeout(()=>{  
     fetch('http://localhost:8000/blogs')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-    })
+        .then(res => {
+            console.log(res);
+            if(!res.ok){
+                throw Error('Could not fetch data for that resource');
+            }
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })
+        .catch((err) => {
+            setError(err.message);
+            setIsPending(false);
+        })
+    },1000);
  
 },[]);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading.......</div>}
             {blogs &&  <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
