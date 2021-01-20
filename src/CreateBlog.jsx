c6b6606ea555cb5799d073daa0f67c9eb5c576d9
mic7x4 <mic7x4@gmail.com>
2021-01-20 12:53:20 +0200

    [feature] adding a custom form for a blog post

--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react';
 
 function CreateBlog() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('crook');
     return (
         <div>
             <div className="createBlog">
                 <h2>Add a new Blog</h2>
+                <form></form>
+                    <label>Blog title:</label>
+                    <input 
+                        type="text"
+                        required
+                        value={title}
+                        onChange= {(e)=>setTitle(e.target.value)}
+                    />
+                    <label>Blog Body:</label>
+                    <textarea
+                        value={body}
+                        onChange= {(e)=>setBody(e.target.value)}
+                        required
+                    ></textarea>
+                    <label>Author:</label>
+                    <select 
+                        value={author}
+                        onChange= {(e)=>setAuthor(e.target.value)}
+                    >
+                        <option value="crook">crook</option>
+                        <option value="netphantom">netphantom</option>
+                    </select>
+                    <button>Add Blog</button>
+                    <p>{title}  {body}</p>
+                    <p>{author}</p>
             </div>
         </div>
     )
